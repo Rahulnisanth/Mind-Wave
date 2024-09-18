58ced7495349d6a48bff49882027dac5bb10a950
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-09-18 16:10:09 +0530

    added: mind-wave project setup using react native expo.

---
+++ app/index.jsx
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
