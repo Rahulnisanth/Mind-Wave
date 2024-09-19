db7d4ec3f4e2cecf4d7381145fdd201c46f3a06d
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-09-19 12:29:53 +0530

    added: project assets configured: native-wind config files

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,9 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 justify-center items-center">
       <Text>Hello World!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/(tabs)/meditate.tsx",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        rmono: ["Roboto-Mono", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
