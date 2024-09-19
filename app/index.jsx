59673cf2286b7f4bcedd15748db81b30059a607c
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-09-19 15:37:08 +0530

    added: app icon & splash png started with the initial page setup

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,41 @@
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
+import beachImage from "@/assets/meditation-images/beach.webp";
+import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView } from "react-native";
+import CustomButton from "../components/CustomButton";
+import { StatusBar } from "expo-status-bar";
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Hello World!</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <LinearGradient
+          className="flex-1"
+          colors={["rgba(0,0,0,0.4)", "rgba(0, 0, 0, 0.8)"]}
+        >
+          <SafeAreaView className="flex-1 mx-5 my-12 justify-between">
+            <View>
+              <Text className="text-center text-white text-4xl font-bold uppercase">
+                Mind Wave
+              </Text>
+              <Text className="text-center mt-3 text-white text-regular text-2xl font-semibold">
+                Simplifying the process of meditation
+              </Text>
+            </View>
+            <View>
+              <CustomButton
+                onPress={() => console.log("Tap")}
+                title="Get Started"
+              />
+            </View>
+            <StatusBar style="light" />
+          </SafeAreaView>
+        </LinearGradient>
+      </ImageBackground>
     </View>
   );
 }
---
+++ components/CustomButton.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
+
+const CustomButton = ({
+  title,
+  onPress,
+  textStyles = "",
+  buttonStyles = "",
+}) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      onPress={onPress}
+      className={`bg-white rounded-xl min-h-[62px] justify-center items-center ${buttonStyles}`}
+    >
+      <Text className={`font-semibold text-lg ${textStyles}`}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
