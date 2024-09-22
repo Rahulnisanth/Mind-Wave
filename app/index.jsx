e058db00f0aa99e33a0e2bbbd8d4fc3d7f421caf
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-09-22 21:43:28 +0530

    fixed the index png

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,10 @@
-import { View, ImageBackground, Text, SafeAreaView } from "react-native";
+import { View, ImageBackground, Text, SafeAreaView, Image } from "react-native";
 import beachImage from "@/assets/meditation-images/beach.webp";
 import CustomButton from "../components/CustomButton";
 import { StatusBar } from "expo-status-bar";
 import { useRouter } from "expo-router";
 import AppGradient from "../components/AppGradient";
+import MeditationLogo from "@/assets/indexMeditationLogo.png";
 
 export default function HomeScreen() {
   const router = useRouter();
@@ -17,7 +18,7 @@ export default function HomeScreen() {
         <AppGradient colors={["rgba(0,0,0,0.5)", "rgba(0, 0, 0, 0.9)"]}>
           <SafeAreaView className="flex-1 justify-center items-center px-3 py-12">
             <SafeAreaView className="flex-1 mx-5 my-12 justify-between">
-              <View>
+              <View className="flex-grow items-center">
                 <Text className="text-white text-center text-5xl font-extrabold tracking-widest">
                   Mind Wave
                 </Text>
@@ -25,12 +26,22 @@ export default function HomeScreen() {
                   Simplifying the process of meditation
                 </Text>
               </View>
-              <View>
+
+              <View className="flex-1 justify-center items-center">
+                <Image
+                  source={MeditationLogo}
+                  resizeMode="cover"
+                  className="h-64 w-64"
+                />
+              </View>
+
+              <View className="mb-10">
                 <CustomButton
                   onPress={() => router.push("/nature-meditate")}
                   title="Get Started"
                 />
               </View>
+
               <StatusBar style="light" />
             </SafeAreaView>
           </SafeAreaView>
